ded50019f2c9874e19f2da5e725c7279096678cc
ROSHAN TIRKEY <roshant.ug21.ec@nitp.ac.in>
2023-09-24 11:04:43 +0530

    finally almost completed

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -13,11 +13,45 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [Text('hello')],
-      ),
-    );
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              children: [
+                Card(
+                  margin: const EdgeInsets.all(15),
+                  elevation: 9,
+                  child: SizedBox(
+                    height: 650,
+                    width: MediaQuery.of(context).size.width * 5,
+                    child: const Image(
+                      image: AssetImage(
+                        'images/map.jpeg',
+                      ),
+                      fit: BoxFit.fitHeight,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: SizedBox(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.search),
+                            hintText: 'Search',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5)))),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
